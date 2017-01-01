107cba2208720ad3e5b56d23c9b607841a9e86c5
Morten Delenk <morten@dark32.cf>
2017-01-01 13:12:46 +0000

    cleared repo

--- kernel/interrupts.cpp
+++
@@ -1,7 +0,0 @@
-#include <interrupts.h>
-#include <stdint.h>
-namespace MTGos {
-const auto Interrupt::syscall(unsigned int callnum, void * handle, void *args) -> unsigned int {
-    return 0;
-}
-}

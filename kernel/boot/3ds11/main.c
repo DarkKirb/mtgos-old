823392756f7d0d46fa81d14f726f851e67ffba0f
Morten Delenk <morten@dark32.cf>
2017-01-29 14:06:37 +0000

    added N3DS support. also added exception vectors which do nothing

--- kernel/boot/3ds11/main.c
+++ kernel/boot/3ds11/main.c
@@ -1,5 +1,16 @@
 #include <multiboot.h>
+#include <stdint.h>
 void start(int eax, multiboot_info_t* ebx);
-void _start(int eax, multiboot_info_t* ebx) {
-    start(eax,ebx);
+void n3ds_init(int eax, multiboot_info_t* ebx) {
+    if((*((uint32_t*)0x10140FFC))&2==0)
+        return; //Old 3DS
+    //New 3ds. Bit 2 says whether we can use 3x or 2x clock speed.
+    *((uint16_t*)0x10141300) = 1;
+    if((*((uint32_t*)0x10140FFC))&4==0)
+        *((uint16_t*)0x10141300) |= 3; //500 MHz
+    else
+        *((uint16_t*)0x10141300) |= 5; //800 MHz
+    //Activate the Additional 128MB MMAP
+    multiboot_memory_map_t *mmap=(multiboot_memory_map_t*)(ebx->mmap_addr);
+    mmap[6].type=MULTIBOOT_MEMORY_AVAILABLE;
 }
--- kernel/boot/3ds9/main.c
+++ kernel/boot/3ds9/main.c
@@ -1,5 +1,12 @@
 #include <multiboot.h>
+#include <stdint.h>
 void start(int eax, multiboot_info_t* ebx);
-void _start(int eax, multiboot_info_t* ebx) {
-    start(eax,ebx);
+void n3ds_init(int eax, multiboot_info_t* ebx) {
+    if((*((uint32_t*)0x10140FFC))&2==0)
+        return; //Old 3DS
+    //New 3ds. Activate the 512KiB of ram we additionally get.
+    *((uint32_t*)0x10000200) |= 1;
+    //Activate the 512KiB mmap
+    multiboot_memory_map_t *mmap=(multiboot_memory_map_t*)(ebx->mmap_addr);
+    mmap[2].type=MULTIBOOT_MEMORY_AVAILABLE;
 }

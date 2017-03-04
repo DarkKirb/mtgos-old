4e1c67114b7da32a9eb33d02afcefd544253284b
Morten Delenk <morten@dark32.cf>
2017-03-04 10:39:03 +0000

    more autopep8

--- genfont.py
+++ genfont.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import binascii
-f=open("font.bin","wb")
-f2=open("font.big","wb")
+f = open("font.bin", "wb")
+f2 = open("font.big", "wb")
 f.write(b"TNOF")
 f2.write(b"FONT")
-fin=open("unifont.hex","r")
-lines=fin.readlines()
-font={}
+fin = open("unifont.hex", "r")
+lines = fin.readlines()
+font = {}
 for l in lines:
     font[int.from_bytes(binascii.unhexlify(
         l[:4]), byteorder="big")] = binascii.unhexlify(l[5:-1])
@@ -29,15 +29,17 @@ for id, fo in font.items():
     else:
         f.write(int(16).to_bytes(4, byteorder="little"))
         f2.write(int(16).to_bytes(4, byteorder="big"))
-    temp=b''
+    temp = b''
     for i in range(16):
-        b='{:016b}'.format(int.from_bytes(bytes([fo[i*2+1],fo[i*2]]),byteorder="little"))
-        temp+=int(b[::-1],2).to_bytes(2,byteorder="little")
-    f.write(id.to_bytes(4,byteorder="little"))
-    f2.write(id.to_bytes(4,byteorder="big"))
-        b='{:016b}'.format(int.from_bytes(bytes([fo[i*2+1],fo[i*2]]),byteorder="little"))
-        temp+=int(b[::-1],2).to_bytes(2,byteorder="little")
-    f.write(id.to_bytes(4,byteorder="little"))
+        b = '{:016b}'.format(int.from_bytes(
+            bytes([fo[i * 2 + 1], fo[i * 2]]), byteorder="little"))
+        temp += int(b[::-1], 2).to_bytes(2, byteorder="little")
+    f.write(id.to_bytes(4, byteorder="little"))
+    f2.write(id.to_bytes(4, byteorder="big"))
+        b = '{:016b}'.format(int.from_bytes(
+            bytes([fo[i * 2 + 1], fo[i * 2]]), byteorder="little"))
+        temp += int(b[::-1], 2).to_bytes(2, byteorder="little")
+    f.write(id.to_bytes(4, byteorder="little"))
         b = '{:016b}'.format(int.from_bytes(
             bytes([fo[i * 2 + 1], fo[i * 2]]), byteorder="little"))
         temp += int(b[::-1], 2).to_bytes(2, byteorder="little")

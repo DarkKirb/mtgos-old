9c5800ae6818ea78e6fd08a6a7e5ce3f9fba8c48
Morten Delenk <morten@dark32.cf>
2017-03-04 10:37:15 +0000

    Autopep8

--- genfont.py
+++ genfont.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 import binascii
-f=open("font.bin","wb")
+f = open("font.bin", "wb")
 f.write(b"FONT")
-fin=open("unifont.hex","r")
-lines=fin.readlines()
-font={}
+fin = open("unifont.hex", "r")
+lines = fin.readlines()
+font = {}
 for l in lines:
-    font[int.from_bytes(binascii.unhexlify(l[:4]),byteorder="big")] = binascii.unhexlify(l[5:-1])
+    font[int.from_bytes(binascii.unhexlify(
+        l[:4]), byteorder="big")] = binascii.unhexlify(l[5:-1])
 f.write(len(font).to_bytes(4, byteorder="little"))
 f.write(int(16).to_bytes(4, byteorder="little"))
 f.write(int(0).to_bytes(4, byteorder="little"))
 for id, fo in font.items():
-    fullwidth=True
-    if len(fo)==16:
-        fullwidth=False
-        temp=b''
+    fullwidth = True
+    if len(fo) == 16:
+        fullwidth = False
+        temp = b''
         for b in fo:
-            temp+=bytes([b,0])
-        fo=temp
+            temp += bytes([b, 0])
+        fo = temp
         f.write(int(8).to_bytes(4, byteorder="little"))
     else:
         f.write(int(16).to_bytes(4, byteorder="little"))
-    temp=b''
+    temp = b''
     for i in range(16):
-        b='{:016b}'.format(int.from_bytes(bytes([fo[i*2+1],fo[i*2]]),byteorder="little"))
-        temp+=int(b[::-1],2).to_bytes(2,byteorder="little")
-    f.write(id.to_bytes(4,byteorder="little"))
+        b = '{:016b}'.format(int.from_bytes(
+            bytes([fo[i * 2 + 1], fo[i * 2]]), byteorder="little"))
+        temp += int(b[::-1], 2).to_bytes(2, byteorder="little")
+    f.write(id.to_bytes(4, byteorder="little"))
     f.write(temp)
 f.close()
 fin.close()
--- make.py
+++ make.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-################################################################################
+##########################################################################
 #  MTGos - A nanokernel in C++
 #  Copyright (C) 2017 Morten Delenk
 #
@@ -15,80 +15,99 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+##########################################################################
 import yaml
 import argparse
 import fnmatch
 import os
 import shutil
 import random
+
+
 def main():
     parser = argparse.ArgumentParser(description="Build MTGos")
+
     def addBinary(flag, help, dest):
-        parser.add_argument(flag, help=help, dest=dest, action="store_const", const=True, default=False)
+        parser.add_argument(flag, help=help, dest=dest,
+                            action="store_const", const=True, default=False)
     addBinary("--clean", "clean all temporary files", "clean")
-    addBinary("--reset", "reset configuration","reset")
-    addBinary("--ci", "force build","ci")
-    parser.add_argument("--set-arch", help="Clean all files and copy the template", dest="archcopy")
-    args=parser.parse_args()
+    addBinary("--reset", "reset configuration", "reset")
+    addBinary("--ci", "force build", "ci")
+    parser.add_argument(
+        "--set-arch", help="Clean all files and copy the template", dest="archcopy")
+    args = parser.parse_args()
     print(args)
     if args.clean:
         clean()
     if args.archcopy != None:
-        shutil.copy("./templates/{arch}.yaml".format(arch=args.archcopy),"./config.yaml")
+        shutil.copy(
+            "./templates/{arch}.yaml".format(arch=args.archcopy), "./config.yaml")
     if not os.access("config.yaml", os.F_OK):
-        raise ValueError("You need to select a profile before you can build MTGos!")
+        raise ValueError(
+            "You need to select a profile before you can build MTGos!")
     with open("config.yaml") as f:
-        config=yaml.load(f)
+        config = yaml.load(f)
     if args.reset:
-        shutil.copy("./templates/{arch}.yaml".format(arch=config["arch"]),"./config.yaml")
+        shutil.copy(
+            "./templates/{arch}.yaml".format(arch=config["arch"]), "./config.yaml")
         with open("config.yaml") as f:
-            config=yaml.load(f)
+            config = yaml.load(f)
     if not config["finished"] and not args.ci:
-        raise ValueError("You have to finish your configuration before you can build!")
-    buildid=" -DBUILDID="+hex(random.getrandbits(64))+" "
-    config["cflags"]+=buildid
-    config["cxxflags"]+=buildid
-    config["asflags"]+=buildid
+        raise ValueError(
+            "You have to finish your configuration before you can build!")
+    buildid = " -DBUILDID=" + hex(random.getrandbits(64)) + " "
+    config["cflags"] += buildid
+    config["cxxflags"] += buildid
+    config["asflags"] += buildid
     print("Parsing the kernel config file")
-    flags=getdefs(config)
-    config["cflags"]+=flags
-    config["cxxflags"]+=flags
-    config["asflags"]+=flags
+    flags = getdefs(config)
+    config["cflags"] += flags
+    config["cxxflags"] += flags
+    config["asflags"] += flags
     print("Starting build")
-    wd=os.getcwd()
+    wd = os.getcwd()
     for d in config["search_dirs"]:
         os.chdir(d)
-        print("Building in "+d)
+        print("Building in " + d)
         build(config)
         os.chdir(wd)
     print("Linking mtgos")
-    link(config,"mtgos.elf", "kernel")
+    link(config, "mtgos.elf", "kernel")
     print("Creating ISO")
-    shutil.copy("mtgos.elf","iso")
-    shutil.copy("font.bin","iso")
+    shutil.copy("mtgos.elf", "iso")
+    shutil.copy("font.bin", "iso")
     os.system("grub-mkrescue -o bootable.iso iso")
     print("All done!")
-#Cleans all *.o files
+# Cleans all *.o files
+
+
 def clean():
     for root, dirnames, filenames in os.walk('.'):
         for filename in fnmatch.filter(filenames, '*.o'):
             os.remove(os.path.join(root, filename))
+
+
 def getCFlags(config):
-    cflags=config["cflags"]
+    cflags = config["cflags"]
     for f in config["includes"]:
-        cflags+=" -I"+f
+        cflags += " -I" + f
     return cflags
+
+
 def getCXXFlags(config):
-    cflags=config["cxxflags"]
+    cflags = config["cxxflags"]
     for f in config["includes"]:
-        cflags+=" -I"+f
+        cflags += " -I" + f
     return cflags
+
+
 def getASFlags(config):
-    cflags=config["asflags"]
+    cflags = config["asflags"]
     for f in config["includes"]:
-        cflags+=" -I"+f
+        cflags += " -I" + f
     return cflags
+
+
 def build(config):
     for root, dirnames, filenames in os.walk('.'):
         for filename in fnmatch.filter(filenames, '*.c'):
@@ -97,66 +116,76 @@ def build(config):
             compileCXX(config, os.path.join(root, filename))
         for filename in fnmatch.filter(filenames, '*.S'):
             assemble(config, os.path.join(root, filename))
+
+
 def getdefs(config):
     def getd(builtin, prefix=""):
-        for n,v in builtin.items():
-            if isinstance(v,dict):
-                for f in getd(v,prefix+n+"_"):
+        for n, v in builtin.items():
+            if isinstance(v, dict):
+                for f in getd(v, prefix + n + "_"):
                     yield f
             else:
                 if v:
-                    print("Using flag -D"+prefix+n)
-                    yield prefix+n
-    flags=""
+                    print("Using flag -D" + prefix + n)
+                    yield prefix + n
+    flags = ""
     for f in getd(config["builtins"]):
-        flags+="-D"+f+" "
+        flags += "-D" + f + " "
     return flags
 
+
 def compileC(config, f):
-    cflags=getCFlags(config)
+    cflags = getCFlags(config)
     try:
-        if os.stat(f).st_mtime < os.stat(f+".o").st_mtime:
-            print(f+" is up to date.")
+        if os.stat(f).st_mtime < os.stat(f + ".o").st_mtime:
+            print(f + " is up to date.")
             return
     except:
         pass
-    print("Compiling C file "+f)
-    if os.system("{prefix}gcc {cflags} -c -o {f}.o {f}".format(prefix=config["prefix"],cflags=cflags,f=f)):
-        raise ValueError("Failed compiling {f} via {prefix}gcc {cflags} -c -o {f}.o {f}".format(prefix=config["prefix"],cflags=cflags,f=f))
+    print("Compiling C file " + f)
+    if os.system("{prefix}gcc {cflags} -c -o {f}.o {f}".format(prefix=config["prefix"], cflags=cflags, f=f)):
+        raise ValueError("Failed compiling {f} via {prefix}gcc {cflags} -c -o {f}.o {f}".format(
+            prefix=config["prefix"], cflags=cflags, f=f))
+
 
 def compileCXX(config, f):
-    cxxflags=getCXXFlags(config)
+    cxxflags = getCXXFlags(config)
     try:
-        if os.stat(f).st_mtime < os.stat(f+".o").st_mtime:
-            print(f+" is up to date.")
+        if os.stat(f).st_mtime < os.stat(f + ".o").st_mtime:
+            print(f + " is up to date.")
             return
     except:
         pass
-    print("Compiling C++ file "+f)
-    if os.system("{prefix}g++ {cxxflags} -c -o {f}.o {f}".format(prefix=config["prefix"],cxxflags=cxxflags,f=f)):
-        raise ValueError("Failed compiling {f} via {prefix}g++ {cxxflags} -c -o {f}.o {f}".format(prefix=config["prefix"],cxxflags=cxxflags,f=f))
+    print("Compiling C++ file " + f)
+    if os.system("{prefix}g++ {cxxflags} -c -o {f}.o {f}".format(prefix=config["prefix"], cxxflags=cxxflags, f=f)):
+        raise ValueError("Failed compiling {f} via {prefix}g++ {cxxflags} -c -o {f}.o {f}".format(
+            prefix=config["prefix"], cxxflags=cxxflags, f=f))
+
 
 def assemble(config, f):
-    asflags=getASFlags(config)
+    asflags = getASFlags(config)
     try:
-        if os.stat(f).st_mtime < os.stat(f+".o").st_mtime:
-            print(f+" is up to date.")
+        if os.stat(f).st_mtime < os.stat(f + ".o").st_mtime:
+            print(f + " is up to date.")
             return
     except:
         pass
-    print("Assembling file "+f)
-    if os.system("{prefix}gcc {asflags} -c -o {f}.o {f}".format(prefix=config["prefix"],asflags=asflags,f=f)):
-        raise ValueError("Failed assembling {f} via {prefix}gcc {asflags} -c -o {f}.o {f}".format(prefix=config["prefix"],asflags=asflags,f=f))
+    print("Assembling file " + f)
+    if os.system("{prefix}gcc {asflags} -c -o {f}.o {f}".format(prefix=config["prefix"], asflags=asflags, f=f)):
+        raise ValueError("Failed assembling {f} via {prefix}gcc {asflags} -c -o {f}.o {f}".format(
+            prefix=config["prefix"], asflags=asflags, f=f))
 
-def link(config,f,d):
-    print("Linking "+f)
-    objs=[]
+
+def link(config, f, d):
+    print("Linking " + f)
+    objs = []
     for root, dirnames, filenames in os.walk(d):
         for filename in fnmatch.filter(filenames, '*.o'):
             objs.append(os.path.join(root, filename))
-    objs=' '.join(objs)
-    if os.system("{prefix}g++ {ldflags} -o {f} {objs} -lgcc".format(prefix=config["prefix"],ldflags=config["ldflags"],f=f,objs=objs)):
-        raise ValueError("Failed linking {f} via {prefix}g++ {ldflags} -o {f} {objs} -lgcc".format(prefix=config["prefix"],ldflags=config["ldflags"],f=f,objs=objs))
+    objs = ' '.join(objs)
+    if os.system("{prefix}g++ {ldflags} -o {f} {objs} -lgcc".format(prefix=config["prefix"], ldflags=config["ldflags"], f=f, objs=objs)):
+        raise ValueError("Failed linking {f} via {prefix}g++ {ldflags} -o {f} {objs} -lgcc".format(
+            prefix=config["prefix"], ldflags=config["ldflags"], f=f, objs=objs))
 
 if __name__ == "__main__":
     main()
